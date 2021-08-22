fa934b6c89e647ea8acd36ed2b82e183761bdda9
lxchuan12 <lxchuan12@163.com>
2021-08-23 01:30:14 +0800

    docs: release 文章添加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -218,6 +218,7 @@ module.exports = {
 				collapsable: true, // 可选的, 默认值是 true,
 				sidebarDepth: 2, // 可选的, 默认值是 1
 				children: [
+					"/vue-next-release/",
 					"/vue-next-utils/",
 					"/vue-devtools/",
 					"/vuex-this/",
