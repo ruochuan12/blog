5b03cd248173b4fac3e1a1be7fdd69e511db0e86
ruochuan <lxchuan12@163.com>
2021-12-23 20:17:00 +0800

    feat: 🎸 新增 delay 文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -236,6 +236,7 @@ module.exports = {
 				collapsable: true, // 可选的, 默认值是 true,
 				sidebarDepth: 2, // 可选的, 默认值是 1
 				children: [
+					'/delay/',
 					'/install-pkg/',
 					'/read-pkg/',
 					'/only-allow/',
