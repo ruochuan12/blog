b6bccd70115e56708d39023653e65b0accf3d956
ruochuan <lxchuan12@163.com>
2022-01-05 14:39:16 +0800

    feat: 🎸 新增 dotenv 文章 config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -236,6 +236,7 @@ module.exports = {
 				collapsable: true, // 可选的, 默认值是 true,
 				sidebarDepth: 2, // 可选的, 默认值是 1
 				children: [
+					'/dotenv/',
 					'/delay/',
 					'/install-pkg/',
 					'/read-pkg/',
