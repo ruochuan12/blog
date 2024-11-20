04a53a883977570053f529904fea08e2919b632c
ruochuan <lxchuan12@163.com>
2024-11-21 00:18:29 +0800

    chore: 格式化 copyIndex 文件

--- scripts/copyIndex.js
+++ scripts/copyIndex.js
@@ -1,44 +1,41 @@
-import {globby} from 'globby';
-import fs from 'node:fs';
+import { globby } from "globby";
+import fs from "node:fs";
+import chokidar from "chokidar";
 
-import chokidar from 'chokidar';
+const globalPaths = await globby(["docs/**/README.md"]);
 
-const globalPaths = await globby(['docs/**/README.md']);
-
-const __DEV__ = process.argv.includes('--watch');
+const __DEV__ = process.argv.includes("--watch");
 
 const copyFile = (paths = globalPaths) => {
 	paths.forEach((path) => {
-		fs.copyFile(path, path.replace(/README\.md/, 'index.md'), (err) => {
-			if(err){
+		fs.copyFile(path, path.replace(/README\.md/, "index.md"), (err) => {
+			if (err) {
 				console.err(`${path} 复制为 index.md 失败`, err);
-			}
-			else{
+			} else {
 				// console.log(`${path} 复制为 index.md 成功`);
 				// throw err;
 			}
-		})
-	})
-}
+		});
+	});
+};
 
 const watch = () => {
 	chokidar
-	.watch(globalPaths, {
-		ignored: ['**/.{gitkeep,DS_Store}', '**/**/index.md'],
-	})
-	.on('change', (filePath) => {
-		console.log(filePath, '修改成功');
-		copyFile([filePath]);
-	})
-	.on('ready', async () => {
-		console.log('ready');
-		copyFile();
-	});
-}
+		.watch(globalPaths, {
+			ignored: ["**/.{gitkeep,DS_Store}", "**/**/index.md"],
+		})
+		.on("change", (filePath) => {
+			console.log(filePath, "修改成功");
+			copyFile([filePath]);
+		})
+		.on("ready", async () => {
+			console.log("ready");
+			copyFile();
+		});
+};
 
-if(__DEV__){
+if (__DEV__) {
 	watch();
-}
-else{
+} else {
 	copyFile();
 }
